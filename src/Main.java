3b5fe94ac70fa6d66e3514da61b1fa1f67e5709c
GeorgDeveloper <ifreyd@yandex.ru>
2023-04-16 21:05:52 +0300

    start function

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-    }
-
-    int[] arr = {1, 2, 3, 54, 89, 79, 46, 51, 23, 15, 46};
-    int[] arr2 = {9, 87, 4, 564, 225, 12, 35, 64, 85, 133, 15};
-
-    public static int[] sumArr(int[] arrayOne, int[] arrayTwo){
+    public static int[] sumArr(int[] arrayOne, int[] arrayTwo) {
         int[] result = new int[arrayOne.length];
-        if(arrayOne.length == arrayTwo.length){
+        if (arrayOne.length == arrayTwo.length) {
             for (int i = 0; i < result.length; i++) {
                 result[i] = arrayOne[i] + arrayTwo[i];
             }
@@ -19,4 +11,21 @@ public class Main {
         return result;
     }
 
+    public static void printArray(int[] arr, String nameF) {
+        System.out.println(nameF +":");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 54, 89, 79, 46, 51, 23, 15, 46};
+        int[] arr2 = {9, 87, 4, 564, 225, 12, 35, 64, 85, 133, 15};
+
+        printArray(arr, "Массив 1");
+        printArray(arr2, "Массив 2");
+
+        printArray(sumArr(arr, arr2), "Сумма маcсивов");
+    }
 }
