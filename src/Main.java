c41518f8aa5f7a97b2fffae8e3dc2a66212cc8e4
GeorgDeveloper <ifreyd@yandex.ru>
2023-04-16 20:59:01 +0300

    function sum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
+
+    int[] arr = {1, 2, 3, 54, 89, 79, 46, 51, 23, 15, 46};
+    int[] arr2 = {9, 87, 4, 564, 225, 12, 35, 64, 85, 133, 15};
+
+    public static int[] sumArr(int[] arrayOne, int[] arrayTwo){
+        int[] result = new int[arrayOne.length];
+        if(arrayOne.length == arrayTwo.length){
+            for (int i = 0; i < result.length; i++) {
+                result[i] = arrayOne[i] + arrayTwo[i];
+            }
+        } else {
+            throw new RuntimeException("Разная длина массивов!");
+        }
+        return result;
+    }
+
 }
